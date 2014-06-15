5d2acd676be9892516fab67830e76e7dcc4d1e1c
katis <joni.katajamaki@gmail.com>
2014-06-15 17:23:35 +0300

    First commit

---
+++ Maybe/Maybe.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katis.Data
+{
+    internal class Ref<T>
+    {
+        private T _v;
+        public T Value { get { return _v; } }
+
+        public Ref(T v)
+        {
+            _v = v;
+        }
+    }
+
+    public static class Maybe
+    {
+        public static Maybe<T> None<T>()
+        {
+            return default(Maybe<T>);
+        }
+
+        public static Maybe<T> Create<T>(T value)
+        {
+            return new Maybe<T>(value);
+        }
+
+        public static Maybe<T> ToMaybe<T>(this T value)
+        {
+            return Maybe.Create(value);
+        }
+    }
+
+    public struct Maybe<T> : IEnumerable<T>
+    {
+        private readonly Ref<T> v;
+
+        public Maybe(T value)
+        {
+            v = (value == null) ? null : new Ref<T>(value);
+        }
+
+        public bool HasSome { get { return (v != null); } }
+
+        public Maybe<U> Map<U>(Func<T, U> converter)
+        {
+            return (v == null) ? default(Maybe<U>) : new Maybe<U>(converter(v.Value));
+        }
+
+        public Maybe<U> FlatMap<U>(Func<T, Maybe<U>> converter)
+        {
+            return (v == null) ? default(Maybe<U>) : converter(v.Value);
+        }
+
+        public Maybe<U> FlatMap<U>(Maybe<U> nextValue)
+        {
+            return (v == null) ? default(Maybe<U>) : nextValue;
+        }
+
+        public Maybe<T> Filter(Predicate<T> pred)
+        {
+            return (v == null || !pred(v.Value)) ? default(Maybe<T>) : new Maybe<T>(v.Value);
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            if (v == null) return;
+            action(v.Value);
+        }
+
+        public T GetOrElse(T defaultValue)
+        {
+            return (v == null) ? defaultValue : v.Value;
+        }
+
+        public Maybe<T> OrElse(Maybe<T> elseValue)
+        {
+            return (v == null) ? elseValue : new Maybe<T>(v.Value);
+        }
+
+        public T GetOrThrow<E>(E ex) where E : Exception
+        {
+            if (v == null) throw ex;
+            else return v.Value;
+        }
+
+        public T GetOrThrow<E>(Func<E> newException) where E : Exception
+        {
+            if (v == null) throw newException();
+            else return v.Value;
+        }
+
+        public U Match<U>(Func<T, U> onSome, Func<U> onNone)
+        {
+            return (v == null) ? onNone() : onSome(v.Value);
+        }
+
+        public void MatchAct(Action<T> onSome, Action onNone)
+        {
+            if (v == null) onNone();
+            else onSome(v.Value);
+        }
+
+        public T[] ToArray()
+        {
+            return (v == null) ? new T[]{} : new T[] { v.Value };
+        }
+
+        public List<T> ToList()
+        {
+            var list = new List<T>();
+            if (v != null) list.Add(v.Value);
+            return list;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (v == null)
+            {
+                yield break;
+            }
+            else
+            {
+                yield return v.Value;
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+            if (v == null)
+            {
+                yield break;
+            }
+            else
+            {
+                yield return v.Value;
+            }
+        }
+    }
+}
---
+++ MaybeTest/MaybeTest.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Katis.Data;
+using System.Collections.Generic;
+
+namespace Katis.Data.MaybeTest
+{
+    [TestClass]
+    public class MaybeTest
+    {
+        [TestMethod]
+        public void TestHasSome()
+        {
+            var none = new Maybe<object>(null);
+            Assert.IsFalse(none.HasSome, "HasSome should be false for none");
+
+            var some = new Maybe<int>(23);
+            Assert.IsTrue(some.HasSome, "HasSome should be true for some");
+        }
+
+        [TestMethod]
+        public void TestOrElse()
+        {
+            var greeting = "Hello ";
+            var world = Maybe.Create("world!");
+            var meatbags = Maybe.Create("meatbags.");
+
+            var message = greeting + world.OrElse(meatbags).GetOrElse("Clarice.");
+
+            Assert.AreEqual("Hello world!", message, "message should equal \"Hello world!\"");
+
+            var none = Maybe.None<string>();
+            var messageNone = greeting + none.OrElse(meatbags).GetOrElse("Clarice.");
+
+            Assert.AreEqual("Hello meatbags.", messageNone, "messageNone should equal \"Hello meatbags.\"");
+
+            var messageNoneNone = greeting + none.OrElse(Maybe.None<string>()).GetOrElse("Clarice.");
+
+            Assert.AreEqual("Hello Clarice.", messageNoneNone, "messageNoneNone should equal \"Hello Clarice.\"");
+        }
+
+        [TestMethod]
+        public void TestForEach()
+        {
+            var none = Maybe.Create((object) null);
+
+            var i = 0;
+            none.ForEach(l => i += 1);
+            Assert.AreEqual(0, i, "Empty May should not run ForEach callback");
+
+            var j = 0;
+            var some = Maybe.Create(2);
+            some.ForEach(n => j += n);
+            Assert.AreEqual(2, j, "May with value should run ForEach callback once");
+        }
+
+        [TestMethod]
+        public void TestMap()
+        {
+            var some = Maybe.Create(0);
+            var one = some.Map(v => v + 1).GetOrElse(0);
+            Assert.AreEqual(1, one, "Map should return new value for some");
+
+            var none = Maybe.None<int>();
+            var zero = none.Map(v => v + 1).GetOrElse(0);
+            Assert.AreEqual(0, zero, "Map should return the else-value for none");
+        }
+
+        [TestMethod]
+        public void TestFlatMap()
+        {
+            var janeDoe = Maybe.Create(new {
+                Name = "Jane Doe",
+                Partner = Maybe.Create(new {
+                    Name = "John Doe",
+                    Job = Maybe.Create("Gardener"),
+                    Pet = Maybe.None<string>()
+                })
+            });
+            var husbandsJob = janeDoe
+                .FlatMap(jane => jane.Partner)
+                .FlatMap(partner => partner.Job)
+                .GetOrElse("");
+
+            Assert.AreEqual("Gardener", husbandsJob, "FlatMap should return the janeDoe.Partner.Job-field");
+
+            var husbandsPet = janeDoe
+                .FlatMap(jane => jane.Partner)
+                .FlatMap(partner => partner.Pet)
+                .GetOrElse("");
+
+            Assert.AreEqual("", husbandsPet, "FlatMap should return an empty string for janeDoe.Partner.Pet");
+        }
+
+        [TestMethod]
+        public void TestFilter()
+        {
+            var some = Maybe.Create("hello");
+            var hello = some.Filter(s => s == "hello").GetOrElse("");
+
+            Assert.AreEqual("hello", hello, "Filter should not filter the value in some");
+
+            var notHello = some.Filter(s => s != "hello").GetOrElse("");
+            Assert.AreEqual("", notHello, "Filter should filter out the \"hello\" in some");
+        }
+
+        [TestMethod]
+        public void TestMatch()
+        {
+            var some = Maybe.Create("Hello");
+            var greeting = some.Match(
+                v => v + " world!",
+                () => "'ola"
+            );
+
+            Assert.AreEqual("Hello world!", greeting, "Match on some should have produced \"Hello world!\"");
+
+            var none = Maybe.None<string>();
+            var noneGreeting = none.Match(
+                v => v + " world!",
+                () => "'ola"
+            );
+
+            Assert.AreEqual("'ola", noneGreeting, "Match on none should have produced \"'ola\"");
+        }
+
+        [TestMethod]
+        public void TestMatchAct()
+        {
+            var greeting = "Hello ";
+            var some = Maybe.Create("world!");
+            some.MatchAct(
+                v => greeting += v,
+                () => greeting += "meatbag."
+            );
+
+            Assert.AreEqual("Hello world!", greeting, "MatchAct on some should change greeting to \"Hello world!\"");
+
+            var greetingNone = "Hello ";
+            var none = Maybe.None<string>();
+            none.MatchAct(
+                v => greetingNone += v,
+                () => greetingNone += "meatbag."
+            ); 
+
+            Assert.AreEqual("Hello meatbag.", greetingNone, "MatchAct on none should change greeting to \"Hello meatbag.\"");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestGetOrElseThrow()
+        {
+            var some = Maybe.Create("hello");
+            some.GetOrThrow(new Exception());
+
+            var none = Maybe.Create((string)null);
+            none.GetOrThrow(new IndexOutOfRangeException());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestGetOrElseThrowLambda()
+        {
+            var some = Maybe.Create("hello");
+            some.GetOrThrow(() => new Exception());
+
+            var none = Maybe.Create((string)null);
+            var s = none.GetOrThrow(() => new IndexOutOfRangeException());
+        }
+
+        [TestMethod]
+        public void TestToArray()
+        {
+            var someArr = Maybe.Create(10).ToArray();
+            Assert.AreEqual(1, someArr.Length, "ToArray of some should return a array with a length of 1");
+            Assert.AreEqual(10, someArr[0], "ToArray of some should contain the value 10");
+
+            var noneArr = Maybe.None<int>().ToArray();
+            Assert.AreEqual(0, noneArr.Length, "ToArray of none should return a array with a length of 0");
+        }
+
+        [TestMethod]
+        public void TestToList()
+        {
+            var someArr = Maybe.Create(10).ToList();
+            Assert.AreEqual(1, someArr.Count, "ToArray of some should return a array with a length of 1");
+            Assert.AreEqual(10, someArr[0], "ToArray of some should contain the value 10");
+
+            var noneArr = Maybe.None<int>().ToList();
+            Assert.AreEqual(0, noneArr.Count, "ToArray of none should return a array with a length of 0");
+        }
+
+        [TestMethod]
+        public void TestEnumerator()
+        {
+            var some = Maybe.Create(1);
+            var i = 0;
+            foreach (var n in some)
+            {
+                i += n;
+            }
+            Assert.AreEqual(1, i, "Iterating over some should modify the i-variable once");
+
+            var none = Maybe.None<int>();
+            var j = 0;
+            foreach (var n in none)
+            {
+                j += n;
+            }
+            Assert.AreEqual(0, j, "Iterating over none should not do anything.");
+        }
+    }
+}
